8b9503040e697efa5e780d9332d5edf77863efa4
WerapongSaejung <werapong.jung@gmail.com>
2020-10-13 10:25:45 +0700

    Create Exercise4_Werapong_S.py

---
+++ assignments/Exercise4_Werapong_S.py
@@ -0,0 +1,13 @@
+Foud = 50   # Foundation
+Genr = 70   # General
+Intr = 75  # Introduction
+Cpt = 90   # Computer
+
+print(" ----------- Your Score --------------")
+
+print("Foundation English Is =              ", Foud)
+print("General Business Is =                ", Genr)
+print("Introduction to Computer Systems Is =", Intr)
+print("Computer Programming Is =            ", Cpt)
+
+print(" -------------------------------------")
