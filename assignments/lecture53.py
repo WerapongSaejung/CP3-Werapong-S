7384f9a41699e0b97f039ee110999f9bd1a1c474
WerapongSaejung <werapong.jung@gmail.com>
2020-11-29 11:02:59 +0700

    Update lecture53.py
    add Comment

--- assignments/lecture53.py
+++ assignments/lecture53.py
@@ -1,5 +1,5 @@
 def vatCalculate(totalPrice):
     result = totalPrice + (totalPrice*7/100)
     return result
-totalPrice = int(input("Input Your Total Price : "))
+totalPrice = int(input("Input Your Total Price : ")) #ประกาศ Input ที่บรรทัดนี้ ชื่อ totalPrice เพื่อส่งค่าไป totalPrice บรรทัดที่ 5
 print(vatCalculate(totalPrice))
