2fb529c91db4a7cf4cc599ffca84f5853fdeb8bb
WerapongSaejung <werapong.jung@gmail.com>
2020-12-13 11:34:04 +0700

    Create Lecture72_Werapong_S.py
    Create First Time

---
+++ assignments/Lecture72_Werapong_S.py
@@ -0,0 +1,17 @@
+menuList = []
+def showBill():
+    totalBill = 0
+    textBill = "Your Bill"
+    print(textBill.center(60,"-"))
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+
+while True :
+    menuName = input("Please Enter Menu :")
+    if (menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Please Enter Your Price :")
+        menuList.append([menuName,menuPrice])
+print(menuList)
+print(showBill())
